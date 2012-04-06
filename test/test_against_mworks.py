40c420e6ff803e5940c63539bf48700b55ef6d5f
Brett Graham <graham@rowland.harvard.edu>
2012-04-06 14:04:18 -0400

    moving test

--- test/test_against_mworks.py
+++ test/test_against_mworks.py
@@ -11,13 +11,13 @@ filename = os.path.realpath(sys.argv[1])
 mf = mworks.data.MWKFile(filename)
 mf.open()
 
-pf = pymworks.IndexedDataFile("%s/%s" % (filename, os.path.basename(filename)))
+pf = pymworks.open_file("%s/%s" % (filename, os.path.basename(filename)))
 
 mcodec = mf.codec
-pcodec = pf.get_codec()
+pcodec = pf.codec
 
-mcodes = [k for (k,v) in mcodec.iteritems() if k >=4]
-pcodes = [k for (k,v) in pcodec.iteritems() if k >=4]
+mcodes = [k for (k, v) in mcodec.iteritems() if k >= 4]
+pcodes = [k for (k, v) in pcodec.iteritems() if k >= 4]
 
 if mcodes != pcodes:
     print "Mcodes and Pcodes differed"
@@ -30,9 +30,9 @@ bad_codes = []
 
 for c in mcodes:
     me = mf.get_events(codes=[c])
-    pe = pf.get_events_by_code(c)
+    pe = pf.get_events(c)
     mtv = [(e.time, e.value) for e in me]
-    ptv = [(p[1], p[2]) for p in pe]
+    ptv = [(e.time, e.value) for e in pe]
     if mtv != ptv:
         bad_codes.append((c, me[0], pe[0]))
         print "Events for code %i did not match" % c
