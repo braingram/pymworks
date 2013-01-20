b53c976e002da88575304da1405ecaeedb25d508
Brett Graham <graham@rowland.harvard.edu>
2013-01-20 15:40:16 -0500

    making lt, gt, ge, le all throw an exception for event comparisons

--- pymworks/events/event.py
+++ pymworks/events/event.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 """
+
+-- Event Comparisons --
+comparisons of events are ill-defined
+for example:
+ e0 = Event(0, 1, 0)
+ e1 = Event(0, 0, 1)
+is e0 > e1?
+ e0.value < e1.value == True
+ e0.time < e1.value == False
+therefore, only compare the parts of each event
+ e0.value > e1.value
+and pass correct keys to other functions:
+ max([e0, e1], key=lambda e: e.time)  # = e0
+ max([e0, e1], key=lambda e: e.value)  # = e1
 """
 
 import collections
+
+
+comparison_error = ArithmeticError( \
+        "Event comparisons are ill-defined, instead compare the code, " \
+        "time or values directly")
 
 
 class Event(collections.Mapping):
@@ -31,3 +50,23 @@ class Event(collections.Mapping):
         return "%s[code=%r, name=%s, time=%r, value=%r]" % \
                 (self.__class__.__name__, self.code, self.name, \
                 self.time, self.value)
+
+    def __eq__(self, other):
+        return (self.name == other.name) and (self.time == other.name) and \
+                (self.value == other.value)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    # comparisons of events are ill-defined
+    def __lt__(self, other):
+        raise comparison_error
+
+    def __le__(self, other):
+        raise comparison_error
+
+    def __gt__(self, other):
+        raise comparison_error
+
+    def __ge__(self, other):
+        raise comparison_error
