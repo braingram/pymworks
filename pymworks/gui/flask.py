5d00c095580e6b19a8b12003e1c7b807f60c53d1
Brett Graham <graham@rowland.harvard.edu>
2013-01-22 15:43:17 -0500

    continued work on flask gui, now making things more modular

--- pymworks/gui/flask.py
+++ pymworks/gui/flask.py
@@ -24,91 +24,37 @@ Return format:
 """
 
 
-import json
 import os
 
 import flask
 
-
-def process_request(data, obj):
-    if ('func' in data) and ('attr' in data):
-        raise ValueError("Request must not contain func[%s] and attr[%s]" % \
-                (data['func'], data['attr']))
-
-    if 'attr' in data:
-        attr = data.get('attr', '')
-        if not hasattr(obj, attr):
-            raise AttributeError("Object %s does not have attr %s" % \
-                    (obj, attr))
-        return getattr(obj, attr)
-
-    if 'func' not in data:
-        raise ValueError("Request must contain either a func or attr")
-    # else 'func' in data
-    func = data.get('func', '')
-    if not hasattr(obj, func):
-        raise AttributeError("Object %s does not have func %s" % \
-                (obj, func))
-    f = getattr(obj, func)
-
-    args_str = data.get('args', '[]')
-    try:
-        args = json.loads(args_str)
-        if not isinstance(args, list):
-            raise ValueError("not a list")
-    except Exception as E:
-        raise ValueError("Failed to decode args [%s]: %s" % (args_str, E))
-
-    kwargs_str = data.get('kwargs', '{}')
-    try:
-        kwargs = json.loads(kwargs_str)
-        if not isinstance(kwargs, dict):
-            raise ValueError("not a dict")
-    except Exception as E:
-        raise ValueError("Failed to decode kwargs [%s]: %s" % (kwargs_str, E))
-
-    return f(*args, **kwargs)
+import flask_filetree
+import flask_ajaxify
 
 
-def ajaxify(obj, name='ajax', register=True, app=None, \
-        template_folder=None, static_folder=None):
-    main_dir = os.path.dirname(os.path.abspath(__name__))
-    print main_dir
-    template_folder = os.path.join(main_dir, 'templates') if \
-            template_folder is None else template_folder
-    print template_folder
-    static_folder = os.path.join(main_dir, 'static') if \
-            static_folder is None else static_folder
-    print static_folder
-
-    ajax = flask.Blueprint(name, name, \
-            template_folder=template_folder, static_folder=static_folder)
-
-    @ajax.route('/')
-    def request():
-        try:
-            r = process_request(flask.request.args, obj)
-            e = ''
-            s = 0
-        except Exception as E:
-            s = 1
-            e = 'PY: %s' % E
-            r = ''
-        print r, e, s
-        return flask.jsonify(status=s, result=json.dumps(r), error=e)
-
-    @ajax.route('/test')
-    def main():
-        return flask.render_template('test.html')
-
-    if register:
-        if app is None:
-            app = flask.Flask(name)
-        app.register_blueprint(ajax, url_prefix='/%s' % name)
-        return ajax, app
-    else:
-        return ajax
+def abs_path(cwd, fn):
+    return os.path.join(os.path.dirname(os.path.abspath(cwd)), fn)
 
 
-def make_app(cm, app=None):
-    return ajaxify(cm, name='cm', app=app)
+def make_client_app(client, app=None):
+    if app is None:
+        app = flask.Flask('test', \
+                template_folder=abs_path(__file__, 'client_templates'), \
+                static_folder=abs_path(__file__, 'static'))
+
+    flask_filetree.make_blueprint(app=app, register=True)
+
+    # TODO make events json encodable
+    flask_ajaxify.make_blueprint(client, app=app, register=True)
+
+    @app.route('/')
+    def default():
+        return flask.render_template('index.html')
+
+    return app
+
+
+if __name__ == '__main__':
+    import pymworks
+    c = pymworks.io.stream.Client()
+    app = make_client_app(
